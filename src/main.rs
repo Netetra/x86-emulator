433f35079d1383bfcccdff2fcfa739cd4596bc2e
Netetra <me@netetra.dev>
2023-03-28 21:10:28 +0900

    jump命令とmovを実装

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-//use std::env;
-use std::io::Read;
-use std::fs::File;
+use std::{
+    io::{Read},
+    fs::File, fmt::Debug,
+};
 
 #[derive(Debug)] 
 struct EmulatorX86 {
+    registers: [u32;8],
     _esp: u32,
     _eflags: u32,
     eip: u32,
@@ -13,48 +15,64 @@ struct EmulatorX86 {
 impl EmulatorX86 {
     fn new(mem_size: usize, esp: u32, eip: u32) -> EmulatorX86 {
         return EmulatorX86 {
+            registers: [0;8],
             _esp: esp,
             _eflags: 0,
             eip: eip,
             memory: vec![0; mem_size],
         }
     }
-    fn status(&self) {
+    fn load_binary(&mut self, file_path: &str) {
+        let mut file = File::open(&file_path).unwrap();
+        file.read(&mut self.memory).unwrap();
+    }
+    fn print_dump(&self) {
         println!("\n{:?}\n", &self);
     }
-    fn get_code8(&self, index:usize) -> &u8 {
-        return &self.memory[self.eip as usize + index];
+    fn get_code<T: Copy>(&self, index:usize) -> T {
+        let bytes = std::mem::size_of::<T>();
+        let base_point = self.eip as usize + index;
+        let code_ptr = &self.memory[base_point..base_point+bytes] as *const [u8] as *const T;
+        let code = unsafe { *code_ptr };
+        return code;
     }
-    fn get_sign_code8(&self, index:usize) -> i8 {
-        return self.memory[self.eip as usize + index] as i8;
-    }
-    fn get_code32(&self, index:usize) -> u32 {
-        let mut ans:u32 = 0;
-        for i in 0..4 {
-            ans = ans << 8;
-            ans |= *self.get_code8(index+i) as u32;
+    fn short_jump(&mut self) {
+        println!("run short op");
+        let diff = self.get_code::<i8>(1);
+        if diff >= 0 {
+            self.eip += (diff + 2) as u32;
+        } else {
+            self.eip -= (diff + 2).abs() as u32;
         }
-        //u32がビッグエンディアンだから
-        return ans.to_be();
+        
+    }
+    fn mov(&mut self) {
+        println!("run mov op");
+        let reg = self.get_code::<u8>(0) - 0xB8;
+        let value = self.get_code::<u32>(1);
+        self.registers[reg as usize] = value;
+        self.eip += 5u32;
+    }
+    fn execute(&mut self) {
+        let code = self.get_code::<u8>(0);
+        match code {
+            0xB8..=0xC0 => self.mov(),
+            0xEB => self.short_jump(),
+            _ => panic!("No op")
+        }
     }
 }
 
 fn main(){
     let mem_size: usize = 512;
     let file_path = "./hoge.bin";
-    let mut emulator = EmulatorX86::new(mem_size, 0x0000, 0x0000);
-
-    let mut file = match File::open(&file_path) {
-        Ok(f) => f ,
-        Err(e) => panic!("{}", e)
-    };
-    file.read(&mut emulator.memory).unwrap();
-
+    let mut emulator = EmulatorX86::new(mem_size, 0x0000, 0);
+    emulator.load_binary(file_path);
+    println!("{}",3u32-2u32);
     loop {
-        //let code = emulator.get_code8(0);
-        let code = emulator.get_code8(0);
-        println!("code: {}",code);
-        emulator.status();
+        emulator.execute();
+        emulator.print_dump();
         if emulator.eip == 0 { break; }
     }
+    
 }
